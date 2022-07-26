88a745ada14c4e9c152bdcd478d3177049caf8e4
shelby <faulconershelby@gmail.com>
2022-07-26 12:57:00 -0700

    sorted out the issues. cube rotates. added button but no hadnler to the page.

--- dynamic-cube/src/components/Cube.js
+++ dynamic-cube/src/components/Cube.js
@@ -5,51 +5,55 @@ import * as THREE from 'three';
 const Cube = (props) => {
 
     const mainCube = () => {
-        const canvas = document.querySelector('#canvas');
-        const renderer = new THREE.WebGLRenderer({canvas});
-
+        // creating a scene
+        const scene = new THREE.Scene();
+        // const canvas = document.querySelector('#canvas');
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth*0.5, window.innerHeight*0.5);
+        document.body.appendChild(renderer.domElement);
+    
         //camera//
         // field of view, vertical direction, in degrees //
         const fov = 75;
         // display aspect of canvas // 
-        const aspect = 2; // the canvas default
+        const aspect = (window.innerWidth / window.innerHeight); // the canvas default
         // near and far represent the rendered space in front of camera
         const near = 0.1;
-        const far = 5;
+        const far =  100;
         // camera "frustrum" - 3D pyramid
         const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-
+    
         // cameras position // 
         camera.position.z = 2;
-
-        // creating a scene
-        const scene = new THREE.Scene();
-
-        const boxWidth = 3;
-        const boxHeight = 3;
-        const boxDepth = 3;
-        const bwSegments = 3;
-        const bhSegments = 3;
-        const bdSegments = 3;
+    
+    
+        const boxWidth = 1;
+        const boxHeight = 1;
+        const boxDepth = 1;
+        const bwSegments = 1;
+        const bhSegments = 1;
+        const bdSegments = 1;
         
         const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth,
             bwSegments, bhSegments, bdSegments);
-
-
-        const material = new THREE.MeshBasicMaterial({color: "#ffccd5"});
-
-        const cube = new THREE.Mesh(geometry, material);
-
-        scene.add(cube);
-
-        renderer.render(scene,camera);
-    };
     
+    
+        const material = new THREE.MeshBasicMaterial({color: "purple"});
+    
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+        
+        const animate = () => {
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            requestAnimationFrame( animate );
+            renderer.render(scene, camera);
+        };
+        animate();
+    };
 
     return (
-        <body>
-            <canvas id="canvas">{mainCube()}</canvas>
-        </body>
+        <canvas id="canvas">{mainCube()}</canvas>
     );
 
 }
