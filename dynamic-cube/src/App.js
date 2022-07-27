63f80c472c4475ce2a4a88151a4de9c143e36083
shelby <faulconershelby@gmail.com>
2022-07-27 11:20:08 -0700

    commit before adding OrbitControls to test.

--- dynamic-cube/src/App.js
+++ dynamic-cube/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import Cube from './components/Cube.js';
 import * as THREE from 'three';
 
 function App() {
+  const [buttonText, setButtonText] = useState("rotation off");
+  let newText = "";
+
+  const rotationButtonTxtToggle = () => {
+    
+    if (buttonText === "rotation off") {
+      newText = "rotation on";
+    } else {
+      newText = "rotation off"
+    }
+    setButtonText(newText);
+  };
+
 
   // toggleRotation function
   // pass in the animate() function
@@ -18,15 +31,30 @@ function App() {
 
   // add on click for the button toggle
   
-
+//   const toggleAnimate = () => {
+//     const animate = true;
+//     if (animate) {
+//         Cube.rotation.x += 0.01;
+//         Cube.rotation.y += 0.01;
+//         requestAnimationFrame( toggleAnimate );
+//         renderer.render(scene, camera);
+//     } else {
+//         Cube.rotation.x += 0;
+//         Cube.rotation.y += 0;
+//         requestAnimationFrame( toggleAnimate );
+//         renderer.render(scene, camera);
+//     }; 
+// };
+// toggleAnimate();
   // next will be to toggle button rotation on click of cube
 
   return (
     <body>
       <main>
         <h1>dynamic cube generator</h1>
-        <button>rotate off</button>
-        <Cube />
+        <button onClick={rotationButtonTxtToggle}>{buttonText}</button>
+        <Cube/>
+        
       </main>
     </body>
   );
--- dynamic-cube/src/components/Cube.js
+++ dynamic-cube/src/components/Cube.js
@@ -2,14 +2,13 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import * as THREE from 'three';
 
-const Cube = (props) => {
 
-    
+const Cube = (props) => {
         // creating a scene
         const scene = new THREE.Scene();
         // const canvas = document.querySelector('#canvas');
         const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth*0.9, window.innerHeight*0.9);
+        renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(renderer.domElement);
     
         //camera//
@@ -44,13 +43,33 @@ const Cube = (props) => {
         scene.add(cube);
         
         const toggleAnimate = () => {
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-            requestAnimationFrame( toggleAnimate );
-            renderer.render(scene, camera);
+            const animate = true;
+            if (animate) {
+                cube.rotation.x += 0.01;
+                cube.rotation.y += 0.01;
+                requestAnimationFrame( toggleAnimate );
+                renderer.render(scene, camera);
+            } else {
+                cube.rotation.x += 0;
+                cube.rotation.y += 0;
+                requestAnimationFrame( toggleAnimate );
+            }; 
         };
         toggleAnimate();
 
+        const onWindowResize = () => {
+            camera.aspect = (window.innerWidth)/(window.innerHeight);
+            camera.updateProjectionMatrix();
+
+            renderer.setSize((window.innerWidth), (window.innerHeight));
+            handleRender();
+        };
+
+        const handleRender = () => {
+            renderer.render(scene, camera);
+        };
+        
+        onWindowResize();
 
     return (
         <canvas id="canvas">{Cube}</canvas>
