6a1a2a8d69ce77821909012ba020b8b91e0eef6d
shelby <faulconershelby@gmail.com>
2022-08-01 14:43:29 -0700

    yay we have a cube that renders. working on the animation toggle button. cleaned up the code so that it is easier to read without vanilla three

--- dynamic-cube/src/App.js
+++ dynamic-cube/src/App.js
@@ -1,14 +1,40 @@
 import './App.css';
-import React, {useState, useEffect, useRef} from 'react';
+import ReactDOM from 'react-dom';
+import React, {useState, useRef} from 'react';
 // import Cube from './components/Cube.js';
-import * as THREE from 'three';
+// import * as THREE from 'three';
 import {Canvas, useFrame} from '@react-three/fiber';
+// import { AmbientLight, BoxGeometry, MeshStandardMaterial } from 'three';
 
-function App() {
+const Cube = () => {
+  let boxWidth = 1;
+  let boxHeight = 1;
+  let boxDepth = 1;
+  let bwSegments = 1;
+  let bhSegments = 1;
+  let bdSegments = 1;
+
+  const cubeRef = useRef(true);
+
+  useFrame(() => {
+    cubeRef.current.rotation.y += 0.005;
+    cubeRef.current.rotation.x += 0.005;
+  });
+
+  return (
+    <mesh ref={cubeRef} rotateX={Math.PI *0.25} rotateY={Math.PI * 0.25}>
+      <boxGeometry args={[boxWidth,boxHeight,boxDepth, bwSegments, bhSegments, bdSegments]} />
+      <meshStandardMaterial color={"#84a98c"}/>
+    </mesh>
+  );
+};
+
+const App = () => {
   const [buttonText, setButtonText] = useState("rotation off");
-  const animate = useRef(true);
+  // const animate = useRef(true);
   let newText = "";
 
+  
   const rotationButtonTxtToggle = () => {
     if (buttonText === "rotation off") {
       newText = "rotation on";
@@ -19,129 +45,27 @@ function App() {
     // setAnimate();
   };
 
-  // const setAnimate = () => {
-  //   animate.current = !animate.current;
-  // };
-
-    const mountRef = useRef(null);
-    
-    useEffect(() => {
-      console.log("inside useEffect");
-    // physical components of cube 
-    
-    // create a scene //
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color("#2f3e46");
-
-    // !! create renderer !! //
-    const renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setPixelRatio(window.devicePixelRatio.pixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    mountRef.current.appendChild(renderer.domElement); // is there a better way to do this with react 
-    
-
-    //camera//
-    // field of view, vertical direction, in degrees //
-    const fov = 75;
-    // display aspect of canvas // 
-    const aspect = (window.innerWidth / window.innerHeight); // the canvas default
-    // near and far represent the rendered space in front of camera
-    const near = 0.1;
-    const far =  100;
-    // camera "frustrum" - 3D pyramid
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    // cameras position // 
-    camera.position.z = 1.5;
-
-    // let controls = new threeOrbitControls(camera, renderer.domElement);
-    // controls.listenToKeyEvents(window);
-    // controls.update();
-    
-    // const controls = new OrbitControls(camera, renderer.domElement);
-    
-    
-    
-    
-    
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const bwSegments = 1;
-    const bhSegments = 1;
-    const bdSegments = 1;
-    
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth,
-      bwSegments, bhSegments, bdSegments);
-      
-      const material = new THREE.MeshBasicMaterial({color: "#84a98c"});
-      const cube = new THREE.Mesh(geometry, material);
-      scene.add(cube);
-      // controls.update();
-
-      // controls !!
-      // const controls = new DragControls( cube, camera, renderer.domElement);
-      // controls.addEventListener('dragstart', function(event)
-      // {
-      //   event.object.material.emissive.set(0x000000);
-      // });
-      
-      const toggleAnimate = () => {
-        if (buttonText === "rotation off") {
-          cube.rotation.x += 0.005;
-            cube.rotation.y += 0.005;
-            requestAnimationFrame( toggleAnimate );
-            // controls.update();
-            renderer.render(scene, camera);
-        } else if (buttonText === "rotation on") {
-            cube.rotation.x = 0;
-            cube.rotation.y = 0;
-            // requestAnimationFrame( !toggleAnimate );
-            renderer.render(scene, camera);
-            requestAnimationFrame(toggleAnimate.paused())
-        }; 
-
-    };
-    toggleAnimate();
-
-    const onWindowResize = () => {
-        camera.aspect = (window.innerWidth)/(window.innerHeight);
-        camera.updateProjectionMatrix();
-        renderer.setSize((window.innerWidth), (window.innerHeight));
-        handleRender();
-    };
-
-
-    const handleRender = () => {
-        renderer.render(scene, camera);
-    };
-    return () => mountRef.current.removeChild(renderer.domElement);
-  }, []);
-
-
-  // toggleRotation function
-  // pass in the animate() function
-  // create a state handler for rotation and set rotation (true/false)
-  // default is on 
-  // button text is "rotate off"
-  // if rotation is turned off
-  // change the rotation speed to x 0/ y 0
-  // change the button text to "rotate on"
-  // if rotation turned back onto true
-  // set rotation speed to x 0.1 /  y 0.1
-
-  // add on click for the button toggle
-  
 
   return (
     <div>
-      <main>
+      <header>
         <h1>dynamic cube generator</h1>
-        <button onClick={rotationButtonTxtToggle}>{buttonText}</button>
-        <div ref={mountRef}></div>
-        {/* {useEffect()} */}
-      </main>
+      </header>
+      <div>
+        <button onClick={() =>
+          {rotationButtonTxtToggle();
+          (Cube.cubeRef.current = !Cube.cubeRef.current)}}>{buttonText}</button>
+      </div>
+      <div id="canvasContainer">
+        <Canvas camera={{position:[1,1,1], zoom:1}} gl={{antialias:false}}>
+          <ambientLight />
+          <directionalLight position={[0,0,3]}/>
+          <Cube animate={Cube.cubeRef} />
+        </Canvas>
+      </div>
     </div>
   );
 }
 
+ReactDOM.render(<App/>, document.getElementById('root'));
 export default App;
