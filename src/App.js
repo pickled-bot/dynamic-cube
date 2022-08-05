5256079860046394105b021c6989a1e073ece036
shelby <faulconershelby@gmail.com>
2022-08-05 16:51:18 -0700

    buttons css mostly

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import './App.css';
 import ReactDOM from 'react-dom';
 import React, {useState, useRef} from 'react';
 // import Cube from './components/Cube.js';
-// import * as THREE from 'three';
+import * as THREE from 'three';
 import {Canvas, useFrame} from '@react-three/fiber';
 // import { AmbientLight, BoxGeometry, MeshStandardMaterial } from 'three';
 import {OrbitControls, softShadows} from '@react-three/drei'
 
+const red = new THREE.Color("#f28482");
+const yellow = new THREE.Color("#f6bd60");
+const blue = new THREE.Color("#118ab2");
+const green = new THREE.Color("#84a98c");
+const orange = new THREE.Color("#f4a261");
+const purple = new THREE.Color("#7678ed");
 
-const CubeRendering = ({animate}) => {
+
+const CubeRendering = ({animate, color}) => {
   return (
     <Cube
-    animate = {animate}/>
+    animate = {animate}
+    color = {color}/>
   );
 };
 // add a propery color
@@ -19,7 +27,7 @@ const CubeRendering = ({animate}) => {
 // onclick for each button to change the cube to that color
 
 
-const Cube = ({animate}) => {
+const Cube = ({animate, color}) => {
   let boxWidth = 1;
   let boxHeight = 1;
   let boxDepth = 1;
@@ -41,7 +49,7 @@ const Cube = ({animate}) => {
     <mesh ref={cubeRef} >
       <boxGeometry args={[boxWidth,boxHeight,boxDepth, bwSegments, bhSegments, bdSegments]} />
       {/* <planeGeometry args={[boxWidth,boxHeight]}/> */}
-      <meshStandardMaterial color={"#84a98c"}/>
+      <meshStandardMaterial color={green} clipShadows={true}/>
       <OrbitControls 
       zoomSpeed={0.25} 
       minZoom={40}
@@ -53,19 +61,54 @@ const Cube = ({animate}) => {
 const ToggleColor = (props) => {
   return (
   <section className="colorButtons">
-    <button id="red">
+    <button id="red" onClick={()=>{
+      props.color = red;
+    }}>
     red
+    </button>
+    <button id="orange">
+    orange
     </button>
     <button id="yellow">
       yellow
     </button>
+    <button id="green">
+      green
+    </button>
     <button id="blue">
       blue
+    </button>
+    <button id="purple">
+      purple
     </button>
   </section>
   )
 };
 
+const Instructions = () => {
+  return (
+    <div id="instructions">
+      <h2>
+      instructions
+      </h2>
+      <p className='grey'>(scroll)</p>
+      <p>
+        click or press on the (rotate off / rotate on) button to toggle rotation animation
+        click or press on a color button to change the color of the cube
+      </p>
+      <p>
+        mobile: use one finger to rotate and turn cube,
+        use two fingers to zoom in or out by pinching and pushing,
+        or keep one finger stationary on the space background
+        and use another finger to move the cube
+      </p>
+      <p>
+        desktop: left click to rotate cute, right click to move cube
+        press buttons on left to adjust elements in cube
+      </p>
+    </div>
+  )
+}
 
 const App = () => {
   const [buttonText, setButtonText] = useState("rotation off");
@@ -94,6 +137,7 @@ const App = () => {
       <header>
         <h1>dynamic cube generator</h1>
       </header>
+      <Instructions />
       <div id="buttons">
         <button id="animateBtn" onClick={() =>
           {rotationButtonTxtToggle();
@@ -103,8 +147,8 @@ const App = () => {
       </div>
       <div id="canvasContainer">
         <Canvas camera={{position:[1,1,1], zoom:300}} gl={{antialias:false}} orthographic shadows dpr={[1,2]}>
-          <ambientLight/>
-          <directionalLight position={[6,3,12]} castShadow intensity={2}/>
+          <ambientLight intensity={0.5}/>
+          <directionalLight position={[3,1,6]} castShadow intensity={0.5}/>
           <CubeRendering animate={animate}/>
         </Canvas>
       </div>
